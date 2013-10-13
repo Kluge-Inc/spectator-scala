758587f261d74d46fa3712caa683c0d6cb159800
giko <adsenceg@ya.ru>
2013-10-13 14:20:19 +0400

    Version model Implemented

--- app/models/Document.scala
+++ app/models/Document.scala
@@ -9,8 +9,8 @@ import play.api.db.slick.Config.driver.simple._
 import slick.lifted.{Join, MappedTypeMapper}
 
 
-case class Document(id: Long, name: String, file: Array[Byte], categoryId: Long)
-case class NewDocument(name: String, file: Array[Byte], categoryId: Long)
+case class Document(id: Long, name: String, categoryId: Long, actualVersionId: Long)
+case class NewDocument(name: String, categoryId: Long, actualVersionId: Long)
 
 
 
@@ -18,13 +18,15 @@ case class NewDocument(name: String, file: Array[Byte], categoryId: Long)
 object Documents extends Table[Document]("DOCUMENT") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name", O.NotNull)
-  def file = column[Array[Byte]]("file", O.NotNull)
-
+  def actualVersionId = column[Long]("actual_version_id", O.Nullable)
   def categoryId = column[Long]("category_id", O.NotNull)
-  def category = foreignKey("CATEGORY_FK", categoryId, Categories)(_.id)
 
-  def * = id ~ name ~ file ~ categoryId <>(Document.apply _, Document.unapply _)
-  def autoInc = name ~ file ~ categoryId returning id
+  def category = foreignKey("CATEGORY_FK", categoryId, Categories)(_.id)
+  def version = foreignKey("VERSION_FK", actualVersionId, Versions)(_.id)
+
+
+  def * = id ~ name ~ categoryId ~ actualVersionId <> (Document.apply _, Document.unapply _)
+  def autoInc = id ~ name ~ categoryId ~ actualVersionId returning id
 
   def getByCategory(id: Long)(implicit s:Session) = {
     val query = for {
